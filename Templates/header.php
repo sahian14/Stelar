6f97cd8531596053b94dc7b9fa65d89e0d425de4
sahian14 <148662656+sahian14@users.noreply.github.com>
2023-11-15 14:54:14 -0500

    Add files via upload

---
+++ Templates/header.php
@@ -0,0 +1,35 @@
+
+<!doctype html>
+<html lang="en">
+
+
+<head>
+  <title>Stelar</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+  
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="shortcut icon" href="./Imagenes/pinguino.png">
+  <link href="https://fonts.googleapis.com/css2?family=Lobster+Two:ital@1&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="estilos-index.css">
+</head>
+
+    <header id="header">
+        <div id="banner">
+          <div class="img-banner" >
+            
+                <div class="info-banner">
+                 <h1>Stelar <hr>
+                     <p>Organización y comunicación institucional</p>
+                 </h1>
+                 <a href="funciones_stelar/login.php" class="btn btn-info" >Ingresar</a>
+                 
+                   
+              </div>
+          </div>
+        </div>
+      </header>
---
+++ Templates/menu.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Stelar</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+  
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="shortcut icon" href="Imagenes/pinguino.png">
+  <link href="https://fonts.googleapis.com/css2?family=Lobster+Two:ital@1&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="estilos-index.css">
+</head>
+
+<nav class="menu">
+        <div class="log-box">
+            <h1>Stelar</h1>
+            <span class="btn-menu"><i class="fas fa-bars"></i></span>
+        </div>
+    
+        <div class="list-container">
+            <ul class="lists">
+                <li><a href="index.php" class="active">Inicio</a></li>
+                <li><a href="nosotros.php">¿Quiénes somos?</a></li>
+                <li><a href="funciones.php">Funciones</a></li>
+                <li><a href="funciones/login.php">Chat</a></li>
+                <li><a href="funciones/login.php">Horarios</a></li>
+            </ul>
+        </div>
+    </nav>
