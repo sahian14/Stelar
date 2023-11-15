4efd95b05b867ceea5e71c52be8b497214883f14
sahian14 <148662656+sahian14@users.noreply.github.com>
2023-11-15 14:53:40 -0500

    Create footer.php

---
+++ Templates/footer.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Stelar</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+  
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="shortcut icon" href="Imagenes/pinguino.png">
+  <link href="https://fonts.googleapis.com/css2?family=Lobster+Two:ital@1&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./css/estilos-footer.css">
+</head>
+<footer class="footer">
+    <div class="container_footer">
+        
+      <div class="footer-row">
+          <div class="links">
+              <img src="Imagenes/pinguino-footer.png" alt="">
+         
+          </div>
+      
+     
+          <div class="links">
+           
+              <h4>Inicio</h4>
+              <ul>
+                  <a href="nosotros.php">¿Quiénes somos?</a>
+                 <a href="funciones.php">Funciones</a>
+                 <a href="funciones/login.php">Ingresa</a>
+              </ul>
+          </div>
+          <div class="links">
+              <h4><P></P>Plataforma</h4>
+              <ul>
+                 <a href="funciones/login.php">Chat</a>
+                 <a href="funciones/login.php">Horarios</a>
+                <a href="funciones/login.php">Material de apoyo</a>
+              </ul>
+          </div>
+          <div class="links">
+              <h4>Contacto</h4>
+              <ul>
+                  <a href="#">Comentarios</a>
+                 <a href="#">Información</a>
+                 <a href="#">Politica de privacidad</a>
+                  <a href="#">Terminos y condiciones</a>
+              </ul>
+          </div>
+          
+          <div class="links">
+              <h4>Redes sociales</h4>
+              <div class="social-links">
+               <a href="#"> <i class="fab fa-facebook-f"></i></a>
+               <a href="#"><i class="fab fa-instagram"></i></a>
+               <a href="#"><i class="fab fa-twitter"></i></a>
+              </div>
+          </div>
+
+
+        
+      </div>
+
+  </div>
+
+  </footer>
